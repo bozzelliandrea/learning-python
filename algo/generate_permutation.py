b9c2462a87e942ec5ce80f735e1dd552ebda67fc
bozzelliandrea <bozzelliandrea@yahoo.it>
2024-01-30 19:00:52 +0100

    backtrack: generate all subsets for input array

--- algo/generate_permutation.py
+++ algo/generate_permutation.py
@@ -1,5 +1,22 @@
 from typing import List
 
+def subsets(nums: List[int]) -> List[List[int]]:
+    ans = []
+    subset = []
+    size = len(nums)
+
+    def backtrack(k: int):
+        if size == k:
+            ans.append(subset.copy())
+        else:
+            backtrack(k + 1)
+            subset.append(nums[k])
+            backtrack(k + 1)
+            subset.pop()
+
+    backtrack(0)
+    return ans
+
 
 def permutation(n):
     chosen: List[bool] = [False] * n
@@ -24,3 +41,5 @@ def permutation(n):
 
 if __name__ == '__main__':
     print(permutation(3))
+    print(subsets([1,2,3]))
+
